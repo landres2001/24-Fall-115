f0bfc16fcb17a284da2b49b985ea27c8e7dfdf19
landres2001 <Liam.Andres@flcc.edu>
2024-08-30 11:47:45 -0400

    Add files via upload

---
+++ 0122_HelloWorld.java
@@ -0,0 +1,11 @@
+// Carrie Krueger
+// 1-22-24
+// This program writes a message to the screen.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!"); //writes to the screen
+
+    }
+}
---
+++ 0124HelloWorldMethod.java
@@ -0,0 +1,34 @@
+/* 
+Name: Carrie Krueger
+Date: 1-24-24
+This program write a message to the screen
+*/
+
+public class HelloWorldMethod {
+    public static void main(String[] args) {
+        
+        printMessage();    
+        
+
+    }
+
+    // this method prints a message to the screen
+    public static void printMessage() {
+        System.out.println("Hello, World!"); //writes to the screen    
+    }
+}
+
+/*
+Where did you struggle with this coding work?
+
+What was easy?
+
+What questions do you still have?
+*/
+
+
+
+
+// 1. write comment and public static void below the main method
+// 2. copy print statement from main method into the new method
+// 3. call the new method in the main method
